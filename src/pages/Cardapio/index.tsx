03cc2440a8002bce43998b5c3c0fcb674f9dd98e
Jean Meira <jean.meira10@hotmail.com>
2022-08-18 22:12:08 -0300

    test

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "../../assets/images/logo.svg";
 
 const Cardapio: React.FC = () => {
-  return <div />;
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+    </main>
+  );
 };
 
 export default Cardapio;
