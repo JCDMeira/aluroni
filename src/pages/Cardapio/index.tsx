33d4579a9a183ef452aa2d2814ee8a390204770f
Jean Meira <jean.meira10@hotmail.com>
2022-08-19 20:32:01 -0300

    feat: add header

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -7,6 +7,9 @@ const Cardapio: React.FC = () => {
       <nav className={styles.menu}>
         <Logo />
       </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>A casa do código e da massa</div>
+      </header>
     </main>
   );
 };
