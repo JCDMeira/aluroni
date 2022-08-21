d7eebbd77d58ef79df8ed2c0e010680f522d7f9d
Jean Meira <jean.meira10@hotmail.com>
2022-08-21 12:01:46 -0300

    feat: update SortItems

--- src/pages/Cardapio/SortItems/index.tsx
+++ src/pages/Cardapio/SortItems/index.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SortItems.module.scss";
 import Options from "./ListOfOptions.json";
 import classNames from "classnames";
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 
 interface SortItemProps {
   canSort: string;
@@ -9,17 +10,37 @@ interface SortItemProps {
 }
 
 const SortItems: React.FC<SortItemProps> = ({ canSort, setCanSort }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const nameSort =
+    canSort && Options.find((option) => option.value === canSort)?.nome;
+
   return (
     <button
       className={classNames({
         [styles.ordenador]: true,
         [styles["ordenador--ativo"]]: canSort !== "",
       })}
+      onClick={() => setIsOpen(!isOpen)}
+      onBlur={() => setIsOpen(false)}
     >
-      <span>Ordenar por</span>
-      <div className={styles.ordenador__opitons}>
+      <span>{nameSort || "Ordenar por"}</span>
+      {isOpen ? (
+        <MdKeyboardArrowUp size={20} />
+      ) : (
+        <MdKeyboardArrowDown size={20} />
+      )}
+      <div
+        className={classNames({
+          [styles.ordenador__options]: true,
+          [styles["ordenador__options--ativo"]]: isOpen,
+        })}
+      >
         {Options.map((option) => (
-          <div className={styles.ordenador__option} key={option.value}>
+          <div
+            className={styles.ordenador__option}
+            key={option.value}
+            onClick={() => setCanSort(option.value)}
+          >
             {option.nome}
           </div>
         ))}
