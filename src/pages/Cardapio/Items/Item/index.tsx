8915a3ee5de9170ee68f0a04be1e1251c8eaf060
Jean Meira <jean.meira10@hotmail.com>
2022-08-21 14:25:17 -0300

    feat: update menu items styles

--- src/pages/Cardapio/Items/Item/index.tsx
+++ src/pages/Cardapio/Items/Item/index.tsx
@@ -1,16 +1,17 @@
 import React from "react";
+import classNames from "classnames";
 import styles from "./Item.module.scss";
 import MenuItems from "../MenuItems.json";
 
 type Props = typeof MenuItems[0];
 
 const Item: React.FC<Props> = (props) => {
-  const { title, description, category, size, serving, price } = props;
+  const { title, description, category, size, serving, price, photo } = props;
 
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem}>
-        <img src={"img"} alt={title} />
+        <img src={photo} alt={title} />
       </div>
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>
@@ -18,7 +19,14 @@ const Item: React.FC<Props> = (props) => {
           <p> {description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__tipo}>{category.label}</div>
+          <div
+            className={classNames({
+              [styles.item__tipo]: true,
+              [styles[`item__tipo__${category.label.toLowerCase()}`]]: true,
+            })}
+          >
+            {category.label}
+          </div>
           <div className={styles.item__porcao}>{size}g</div>
           <div className={styles.item__qtdpessoas}>{`Serve ${serving} ${
             serving === 1 ? "pessoa" : "pessoas"
