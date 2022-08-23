d2686985db2a249cb3ba636eeae4f8cdf2481a13
Jean Meira <jean.meira10@hotmail.com>
2022-08-22 22:19:14 -0300

    feat: add page Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import MenuItem from 'pages/Cardapio/Items/MenuItems.json';
+
+const Home: React.FC = () => {
+  return (
+    <section>
+      <h3>Recomendações da cozinha</h3>
+      <div></div>
+    </section>
+  );
+};
+
+export default Home;
