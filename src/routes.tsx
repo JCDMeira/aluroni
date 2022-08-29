27479f164de506bc4b383ab990b5a1da6ae289df
Jean Meira <jean.meira10@hotmail.com>
2022-08-28 21:10:11 -0300

    refactor: add react.lazy

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import NavMenu from 'components/NavMenu';
 import PageModel from 'components/PageModel';
-import Cardapio from 'pages/Cardapio';
-import Home from 'pages/Home';
-import Sobre from 'pages/Sobre';
 import Footer from 'components/Footer';
 import NotFound from 'pages/NotFound';
 import Prato from 'pages/Prato';
+
+const Cardapio = lazy(() => import('pages/Cardapio'));
+const Home = lazy(() => import('pages/Home'));
+const Sobre = lazy(() => import('pages/Sobre'));
 
 export default function AppRouter() {
   return (
     <main className="container">
       <Router>
-        <NavMenu />
-        <Routes>
-          <Route path="/" element={<PageModel />}>
-            <Route index element={<Home />} />
-            <Route path="cardapio" element={<Cardapio />} />
-            <Route path="sobre" element={<Sobre />} />
-          </Route>
-          <Route path="/prato/:id" element={<Prato />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
+        <Suspense fallback={<p>Loading</p>}>
+          <NavMenu />
+          <Routes>
+            <Route path="/" element={<PageModel />}>
+              <Route index element={<Home />} />
+              <Route path="cardapio" element={<Cardapio />} />
+              <Route path="sobre" element={<Sobre />} />
+            </Route>
+            <Route path="/prato/:id" element={<Prato />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Suspense>
       </Router>
     </main>
   );
